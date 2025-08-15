31b151956d6dc0c625223268204f7eba27fa3bbe
ShivamMathtech <drshivam49@gmail.com>
2025-08-15 18:48:28 +0530

    Header File Basic structure is defined

---
+++ header_Prototype.c
@@ -0,0 +1,23 @@
+/*
+ * sensor.h
+ * Public API for sensor module
+ * Author: Shivam
+ * Created: 2025-08-15
+ */
+#ifndef PROJECT_SENSOR_H
+#define PROJECT_SENSOR_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <stddef.h>
+
+/* API follows... */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PROJECT_SENSOR_H */
